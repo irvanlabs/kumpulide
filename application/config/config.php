48d9fcca2c344aedf1a504a8f73e64c42f652ffb
bokunocode <akunemailwahab@myself.com>
2019-03-21 16:57:40 +0700

    Fix lagi

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/codeIgniter/';
+$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/kumpulide/';
 /*
 |--------------------------------------------------------------------------
 | Index File
