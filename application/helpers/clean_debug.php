4cdc119271693cf989a4850f98a726fc28f40908
bokunocode <akunemailwahab@myself.com>
2019-04-13 20:22:07 +0700

    update helper

--- application/helpers/clean_debug.php
+++ application/helpers/clean_debug.php
@@ -3,19 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if(!function_exists('clean_export')){
 	function clean_export($val) : string {
-		return highlight_string('<?php '.var_export($val,true).' ?>',true);
+		return preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.var_export($val,true).' ?>',true));
 	}
 }
 
 if(!function_exists('clean_dump')){
 	function clean_dump($val) : string {
 		ob_start();var_dump($val);
-		return highlight_string('<?php '.trim(ob_get_clean()).' ?>',true);
+		return preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.trim(ob_get_clean()).' ?>',true));
 	}
 }
 
 if(!function_exists('clean_print')){
-	function pretty_var($val) : string {
-		return highlight_string('<?php '.print_r($val,true).' ?>',true);
+	function clean_print($val) : string {
+		return preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.print_r($val,true).' ?>',true));
 	}
 }
