1e04239ec4a3bfbade8175a0b4cfac346f98be04
bokunocode <akunemailwahab@myself.com>
2019-04-13 21:26:34 +0700

    add style for dump-box

--- application/helpers/clean_debug.php
+++ application/helpers/clean_debug.php
@@ -3,19 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if(!function_exists('clean_export')){
 	function clean_export($val) : string {
-		return preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.var_export($val,true).' ?>',true));
+		return '<div class="dump-box">'.preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.var_export($val,true).' ?>',true)).'</div>';
 	}
 }
 
 if(!function_exists('clean_dump')){
 	function clean_dump($val) : string {
 		ob_start();var_dump($val);
-		return preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.trim(ob_get_clean()).' ?>',true));
+		return '<div class="dump-box">'.preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.trim(ob_get_clean()).' ?>',true)).'</div>';
 	}
 }
 
 if(!function_exists('clean_print')){
 	function clean_print($val) : string {
-		return preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.print_r($val,true).' ?>',true));
+		return '<div class="dump-box">'.preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.print_r($val,true).' ?>',true)).'</div>';
 	}
 }
