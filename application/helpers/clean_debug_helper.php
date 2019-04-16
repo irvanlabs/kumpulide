4cb5b1550cb3315461b187e5ac485231cfe5d182
bokunocode <akunemailwahab@myself.com>
2019-04-16 13:24:12 +0700

    update helper

---
+++ application/helpers/clean_debug_helper.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if(!function_exists('clean_export')){
+	function clean_export($val) {
+		return '<div class="dump-box">'.preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.var_export($val,true).' ?>',true)).'</div>';
+	}
+}
+
+if(!function_exists('clean_dump')){
+	function clean_dump($val) {
+		ob_start();var_dump($val);
+		return '<div class="dump-box">'.preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.trim(ob_get_clean()).' ?>',true)).'</div>';
+	}
+}
+
+if(!function_exists('clean_print')){
+	function clean_print($val) {
+		return '<div class="dump-box">'.preg_replace(['/\&lt;\?php\&nbsp;/','/\?\&gt;/'],'',highlight_string('<?php '.print_r($val,true).' ?>',true)).'</div>';
+	}
+}
